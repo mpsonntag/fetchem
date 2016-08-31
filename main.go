574e8b2d582747e9aee54966f859b2c78190d16e
M. Sonntag <michael.p.sonntag@gmail.com>
2016-08-31 17:19:09 +0200

    add main file

---
+++ main.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2016, Michael Sonntag (sonntag@bio.lmu.de)
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted under the terms of the BSD License. See
+// LICENSE file in the root of the Project.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("fetchem started")
+}
+
